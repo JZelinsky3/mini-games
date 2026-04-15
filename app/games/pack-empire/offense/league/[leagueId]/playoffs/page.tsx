c03dc8aa91a8fee583fe58e8b1b18fc3a964d907
Joseph Zelinsky <zelinskj@email.sc.edu>
2026-04-14 21:38:09 -0400

    NEW LEAGUE MODE 1.00 huge update!

---
+++ app/games/pack-empire/offense/league/[leagueId]/playoffs/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+export default function PlayoffsPage() {
+  return (
+    <div style={{ 
+      padding: '2rem', 
+      color: '#d4e8f8', 
+      textAlign: 'center',
+      minHeight: '100vh',
+      background: '#050a18'
+    }}>
+      <h1>🏆 Playoffs</h1>
+      <p>Playoffs page is coming soon...</p>
+      <p>League ID: [dynamic]</p>
+    </div>
+  );
+}
